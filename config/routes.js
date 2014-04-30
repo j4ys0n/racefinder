86f3618ae59bc925dfefb2cdab97fdc7edb820ae
HUGE | Jayson Jacobs <jjacobs@hugeinc.com>
2014-04-30 11:29:27 -0400

    close import route

--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,7 @@ var race = require( __dirname + '/../controllers/race' );
 
 module.exports = function(app) {
   //app.put( '/api/race', race.createRace );
-  app.put( '/api/races', race.createRaces );
+  //app.put( '/api/races', race.createRaces );
   app.get( '/api/race/link/:link', race.findRaceByLink );
   app.get( '/api/race/:id', race.readRace );
   //app.post( '/api/race/:id', race.updateRace );
